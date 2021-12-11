4ffba02c825056ad10c1d0968223355493f4bff3
Alexander-trol <alessandropopa35@gmail.com>
2021-12-11 12:40:43 +0100

    Nuove cartelle e file
    Aggiunte cartelle di vari linguaggi e file di java (server) aggiunti

---
+++ php/exercise/object_example.php
@@ -0,0 +1,21 @@
+<?php
+    
+    class Car{
+        public $color;
+        public $model;
+
+        public function __construct($color, $model){
+            $this ->color = $color;
+            $this ->model = $model;
+        }
+        public function message(){
+            return "My car is a " . $this->color . " " . $this->model . "!";
+        }
+    }
+
+    $myCar = new Car("black", "volvo");
+    echo $myCar -> message();
+    echo "<br>";
+    $myCar = new Car("red", "toyota");
+    echo $myCar -> message();
+?>
